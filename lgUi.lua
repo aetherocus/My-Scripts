1cb35c7d23295af1864cc1a11b17d80702f35350
aetherocus <aether3001@gmail.com>
2024-11-24 20:42:07 +0700

    Update lgUi.lua

--- lgUi.lua
+++ lgUi.lua
@@ -93,21 +93,15 @@ local RemoteName = Main:CreateInput({
    RemoveTextAfterFocusLost = false,
    Flag = "Input1",
    Callback = function(Text)
-   local splitted = string.split(Text,",")
-   local Keybind = Main:CreateKeybind({
-             Name = splitted[1],
-             CurrentKeybind = splitted[2],
-             HoldToInteract = false,
-             Flag = "keybinded", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-             Callback = function(Keybind)
-                 local UserInputService = game:GetService("UserInputService")
+        local splitted = string.split(Text,",")
+        local UserInputService = game:GetService("UserInputService")
 	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 	local ClientRequest = ReplicatedStorage:WaitForChild("Events"):WaitForChild("ClientRequest")
 
 	local args = {
 		[1] = "custom",
 		[2] = "server2",
-		[3] = Name,
+		[3] = splitted[1],
 		[4] = CFrame.new(322.2025451660156, 1.2266260385513306, -412.837158203125, 0.5210793018341064, 4.975538558937842e-08, -0.8535082936286926, 3.810540505355675e-08, 1, 8.155905106832506e-08, 0.8535082936286926, -7.502200816134064e-08, 0.5210793018341064),
 		[5] = 1
 	}
@@ -119,14 +113,12 @@ local RemoteName = Main:CreateInput({
 
 	-- Detect when the = key is pressed
 	UserInputService.InputBegan:Connect(function(input, gameProcessed)
-		if not gameProcessed then -- Ensure the key press is not processed by the game UI
-			if input.KeyCode == Enum.KeyCode[CurrentKeybind] then
-				sendRequest()
-			end
+	     if not gameProcessed then -- Ensure the key press is not processed by the game UI
+		if input.KeyCode == Enum.KeyCode[splitted[2]] then
+		    sendRequest()
 		end
+	    end
 	end)
-             end,
-        })
    end,
 })
 
